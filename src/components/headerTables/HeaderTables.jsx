b24641974aaf5fc5c1983777f255d1bd5795f66f
szymoks11 <81630705+szymoks11@users.noreply.github.com>
2023-02-06 20:36:17 +0100

    Update HeaderTables.jsx

--- src/components/headerTables/HeaderTables.jsx
+++ src/components/headerTables/HeaderTables.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./header.css"
 import logo  from './logo.png';
-import {Container, Nav, Navbar, NavDropdown, Button} from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Button, Form, FormGroup, FormControl} from 'react-bootstrap';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 const Header_tables = () => {
@@ -43,8 +43,33 @@ const Header_tables = () => {
             <NavDropdown.Item href="#action/3.1">Przestrzeń robocza TaskTalk</NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <Button variant='primary' size='sm' className="btn-create">
-        Utwórz
+
+        {/* <Nav className="me-auto">
+          <NavDropdown variant='primary' size='sm' className="btn-create" title="Utwórz" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Przestrzeń robocza TaskTalk</NavDropdown.Item>
+            <NavDropdown.Divider />  */}
+            {/* dorobic skrypt do zczytywania przestrzeni i dodawania ich do listy */}
+            {/* <NavDropdown.Item href="#action/3.1">Przestrzeń robocza TaskTalk</NavDropdown.Item> 
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.1">Przestrzeń robocza TaskTalk</NavDropdown.Item>
+          </NavDropdown>
+        </Nav> */}
+
+        <Button variant='primary' size='sm' className="btn-create"><NavDropdown title="Utwórz">
+          <NavDropdown.Item>Tytuł tablicy</NavDropdown.Item>
+            <NavDropdown.Divider />
+              <Form>
+                <FormGroup>
+                  <FormControl
+                    type="text"
+                    placeholder='Wpisz tytuł tablicy'
+                  />
+                  <Form.Control type="submit" value="Utwórz"/>
+                </FormGroup> 
+
+                
+              </Form>
+            </NavDropdown>
         </Button>
         <NotificationsActiveIcon/>
         <HelpOutlineIcon/>
